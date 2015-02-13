9a8573f06651c11bfc9179c6cb3c09586da1442f
AMIT <AMIT@AMIT-PC>
2015-02-13 22:17:58 +0530

    done

---
+++ CoreJava/src/com/jcodeworld/corejava/language/MainClass.java
@@ -0,0 +1,13 @@
+/*
+ * This class is the main Class to execute the program for test
+ * 
+ */
+package com.jcodeworld.corejava.language;
+
+public class MainClass {
+	public static void main(String[] args) { 
+      TopLevelClass.InnerLevelClass l = new TopLevelClass().new InnerLevelClass();
+      
+	  System.out.println("git repo");
+	}
+}
