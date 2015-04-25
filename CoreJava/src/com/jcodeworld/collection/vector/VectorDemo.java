1abb571f2eb0cbf899dd1cbabb852495fdc56dc9
AMIT <AMIT@AMIT-PC>
2015-04-25 15:40:07 +0530

    update version 1.0

--- CoreJava/src/com/jcodeworld/collection/vector/VectorDemo.java
+++ CoreJava/src/com/jcodeworld/collection/vector/VectorDemo.java
@@ -6,7 +6,7 @@ public class VectorDemo {
 
 	public static void main(String[] args) {
 		Vector v = new Vector();
-		System.out.println("Size of the vector:" + v.size());
+		System.out.println("Size of the vector:[" + v.size()+"]");
 
 	}
 
