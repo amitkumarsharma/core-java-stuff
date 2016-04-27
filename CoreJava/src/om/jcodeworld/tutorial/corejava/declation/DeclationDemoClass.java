9b3b504cab4004fc8f54c657a85ea34449d6c2a3
Amit Kumar Sharma <asharma193@sapient.com>
2016-04-27 12:28:07 +0530

    CoreJava--> Language --> declaration: Primitive type declaration

---
+++ CoreJava/src/om/jcodeworld/tutorial/corejava/declation/DeclationDemoClass.java
@@ -0,0 +1,14 @@
+package om.jcodeworld.tutorial.corejava.declation;
+
+public class DeclationDemoClass {
+
+	public static void main(String[] args) {
+		// Declaration and Initialization of int primitive data type
+		int intVeriable = 10; 
+				
+		// Declaration and Initialization of float primitive data type "f" as postfix to specify it is float
+		float floatVariable = 20.50f; 
+
+	}
+
+}
